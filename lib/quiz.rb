5321a68efa8ad821396d608e8fc3ddcb886a8da9
megosparta <megosparta@mail.ru>
2016-01-11 14:16:40 +0300

    fix 5 task

--- lib/quiz.rb
+++ lib/quiz.rb
@@ -16,14 +16,8 @@ class Quiz
     title_by_line_base(json)
     word_by_line_base(json)
     word_with_line_end_base(json)
-    # puts Benchmark.measure{ 1000000.times { second(str)  }  }
-    # puts Benchmark.measure{ 1000000.times { second2(str)  }  }
     sorted_strings_base(json)
     eighth_task_sort_base(json)
-    # Benchmark.bm do |x|
-    #   x.report("call function") { strip_punctuation(str)  }
-    #   x.report("in function") { str.gsub(/\p{P}/, '').strip }
-    # end
   end
 
   def title_by_line_base(json)
@@ -35,31 +29,6 @@ class Quiz
       end
     end
   end
-
-  # def title_by_line_base(json)
-  #   @title_by_line = {}
-  #   json.each do |poem|
-  #     poem['text'].split("\n").each do |str|
-  #       line = strip_punctuation(str)
-  #       @title_by_line["#{line}"] = strip_punctuation(poem['title'])
-  #     end
-  #   end
-  # end
-  # def word_by_line_base(json)
-  #   line = key.sub('%WORD%', '').strip.gsub(/[[:punct:]]\z/, '').gsub(/ {2,}/, ' ')
-  #   @word_by_line = {}
-  #   json.each do |poem|
-  #     poem['text'].split("\n").each do |str|
-  #       line = strip_punctuation(str)
-  #       words = line.split
-  #       words.each_with_index do |word, index|
-  #         tmp = words.clone
-  #         tmp.delete_at(index)
-  #         @word_by_line["#{tmp.join(' ')}"] = word
-  #       end
-  #     end
-  #   end
-  # end
 
   def word_by_line_base(json)
     @word_by_line = {}
@@ -121,47 +90,43 @@ class Quiz
     string.gsub(/\p{P}/, '').strip
   end
 
+  TOKEN = 'f73854323b84f268f9ae8ef277c621f8'
+  URIP = URI("http://pushkin.rubyroid.by/quiz")
+
   def call(env)
     if env["REQUEST_PATH"] == "/quiz"
-      # ['200', {}, []]
-      req = Rack::Request.new(env)
-      params = JSON.parse( req.body.read )
-      answer(params)
+      # req = Rack::Request.new(env)
+      params = JSON.parse(Rack::Request.new(env).body.read)
+      answer = ''
+      key = params['question']
+      case params['level']
+      when 1
+        answer = first(key)
+      when 2
+        answer = second(key)
+      when 3, 4
+        answer = third_fourth(key)
+      when 5
+        answer = fifth(key)
+      when 6, 7
+        answer = sixth_seventh(key)
+      when 8
+        answer = eighth(key)
+      end
+      parameters = {
+        answer: answer,
+        token: TOKEN,
+        task_id: params['id']
+      }
+      Net::HTTP.post_form(URIP, parameters)
+      puts params
+      puts parameters
     # elsif env["REQUEST_PATH"] == "/registration"
     #   puts "#{params['token']}"
     #   puts req.body.read
     #   ['200', {}, [{answer: "снежные"}.to_json]]
     end
     ['200', {}, []]
-  end
-
-  TOKEN = 'f73854323b84f268f9ae8ef277c621f8'
-  URIP = URI("http://pushkin.rubyroid.by/quiz")
-  def answer(params)
-    answer = ''
-    key = params['question']
-    case params['level']
-    when 1
-      answer = first(key)
-    when 2
-      answer = second(key)
-    when 3, 4
-      answer = third_fourth(key)
-    when 5
-      answer = fifth(key)
-    when 6, 7
-      answer = sixth_seventh(key)
-    when 8
-      answer = eighth(key)
-    end
-    parameters = {
-      answer: answer,
-      token: TOKEN,
-      task_id:  "#{params['id']}"
-    }
-    Net::HTTP.post_form(URIP, parameters)
-    puts params
-    puts parameters
   end
 
   def first(key)
@@ -172,21 +137,6 @@ class Quiz
     @word_by_line[key.sub('%WORD%', '')]
   end
 
-
-  # def second2(key)
-  #   @word_by_line["#{key.sub('%WORD%', '').strip.gsub(/[[:punct:]]\z/, '').gsub(/ {2,}/, ' ')}"]
-  # end
-
-  # def second3(key)
-  #   line = key.gsub('%WORD%', '').strip.gsub(/[[:punct:]]\z/, '').gsub(/ {2,}/, ' ')
-  #   @word_by_line[line]
-  # end
-
-  # def second4(key)
-  #   line = key.gsub('%WORD%', '').strip.gsub(/[[:punct:]]\z/, '').gsub(/ {2,}/, ' ')
-  #   @word_by_line[line]
-  # end
-
   def third_fourth(keys)
     answer = []
     keys.split("\n").each do |key|
@@ -196,27 +146,15 @@ class Quiz
   end
 
   def fifth(key)
-    # @word_by_line = {}
-    # json.each do |poem|
-    #   poem['text'].split("\n").each do |str|
-    #     line = str.strip.gsub(/[[:punct:]]\z/, '')
-    #     words = line.split
-    #     words.each do |word|
-    #       buf_word = word.gsub(/[[:punct:]]\z/, '')
-    #       key = line.sub(buf_word, '')
-    #       @word_by_line[key] = word
-    #     end
-    #   end
-    # end
     answer = ''
-    line = strip_punctuation(key)
-    words = line.split
-    words.each_with_index do |word, index|
-      tmp = words.clone
-      tmp.delete_at(index)
-      correct_word = @word_by_line["#{tmp.join(' ')}"]
+    words = key.split
+    words.each do |word|
+      buf_word = word.gsub(/[[:punct:]]\z/, '')
+      buf_key = key.sub(buf_word, '')
+      correct_word = @word_by_line[buf_key]
       unless correct_word.nil?
-        answer = "#{correct_word},#{word}"
+        answer = "#{correct_word},#{buf_word}"
+        break
       end
     end
     answer
